9811223f9be5e1d224fcea40b1fc6d7ca698657d
Hanzalah Samana <hanzalahsamana789@gmail.com>
2021-12-02 01:47:15 +0500

    functionality done

--- Screens/Buyer.js
+++ Screens/Buyer.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Cards from './card';
+import firebase from 'firebase';
+import { View, StyleSheet , ScrollView  , SafeAreaView} from 'react-native';
 
-const Buyer = ()=> {
-    return(
-       <>
-       <Cards/>
-       </>
+const Buyer = () => {
+
+    const [sellers, setSellers] = useState([])
+
+
+   
+    useEffect(() => {
+        getDate()
+    }, []);
+
+    const getDate = () => {
+        // console.log('user uid', uid)
+        firebase.firestore().collection('users').get().then((onSnapshot) => {
+            setSellers([]);
+            onSnapshot.forEach((doc) => {
+                setSellers(sellers => [...sellers, doc.data()]);
+                console.log(doc.data())
+                // console.log("products", products)
+
+            });
+        });
+    }
+
+
+    return (
+        // <SafeAreaView style={styles.container}>
+            <View  style={styles.container}>
+                {
+                    sellers.map((item, index) => {
+
+                        return (
+                            <View key={index}>
+                                <Cards name={item.name} uid={item.userUID}/>
+                            </View>
+                        )
+                    })
+                }
+            </View>
+        // </SafeAreaView >
+
 
     );
-    
-}
 
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+
+    },
+
+
+});
 export default Buyer;
